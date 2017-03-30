e143eded94b2ef83fd6bc85aaf986001dc6bd396
Paul Kuttiya <w.kuttiya@gmail.com>
2017-03-29 23:13:30 -0400

    add profile

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var hbs = require('express-handlebars');
-var stylus = require('stylus');
-var nib = require('nib');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    hbs = require('express-handlebars'),
+    stylus = require('stylus'),
+    nib = require('nib');
 
 var index = require('./routes/index');
 
 var app = express();
-
 // view engine setup
 app.engine('hbs', hbs({
   extname: 'hbs',
@@ -31,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,37 @@ var router = express.Router();
 
 var path = require('path'),
     fs = require('fs'),
-    project_path = path.dirname(__dirname),
-    file_path = path.resolve(project_path, "db/projects.json"),
-    projects_db = fs.readFileSync(file_path);
+    db_path = path.dirname(__dirname),
+    project_json = path.resolve(db_path, "db/projects.json"),
+    profile_json = path.resolve(db_path, "db/profile.json"),
+    projects_db = fs.readFileSync(project_json),
+    profile_db = fs.readFileSync(profile_json);
 
-var projects = JSON.parse(projects_db).data;
+var projects = JSON.parse(projects_db).data,
+    profile = JSON.parse(profile_db);
+
+var getIcon = function(icon) {
+    var icon = icon.toString().toLowerCase(),
+        img;
+
+    img = "<img src='/images/icons/" + 
+    icon + ".svg'" + " alt=" + "'" + icon + "'" 
+    + " title=" + "'" + icon + "'" +  "/>";
+  
+  return img;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Paul Kuttiya', projects: projects });
+  //helpers for handlebars helper
+  res.render('index', {
+    title: 'Paul Kuttiya',
+    projects: projects, 
+    profile: profile,
+    helpers: {
+      tag: getIcon
+    },
+  });
 });
 
 router.get('/projects/:id', function(req, res, next) {
